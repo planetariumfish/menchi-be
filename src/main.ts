4b4a81812095d63c9f3a20af963aeb07dc0aa090
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-08-11 11:56:19 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
-import express, { Request, Response, Application } from "express";
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
-const app: Application = express();
+const app = express();
 const PORT = process.env.PORT || 2501;
 
-app.get("/", (req: Request, res: Response): void => {
+const allowedOrigins = ["http://localhost:5173"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
+app.use(express.json());
+
+app.get("/", (req, res) => {
   res.send("Hello Typescript with Node.js!");
 });
 
-app.listen(PORT, (): void => {
+app.listen(PORT, () => {
   console.log(`Server Running here 👉 https://localhost:${PORT}`);
 });
